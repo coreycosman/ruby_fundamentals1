7ebce5000e5274b72146d45cbec43899d12b0ecf
COREY COSMAN <cosman.corey@gmail.com>
2017-09-18 15:56:43 -0400

    edit exercise4b

--- exercise4b.rb
+++ exercise4b.rb
@@ -6,4 +6,6 @@ if user_age < 27
   puts "our age difference is: #{27 - user_age} years"
 elsif user_age > 27
   puts "our age dfference is #{user_age - 27} years"
-end
+else user_age > 105
+  puts "sure!"
+  
